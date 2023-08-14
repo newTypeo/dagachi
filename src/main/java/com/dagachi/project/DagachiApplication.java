e230b2724e87b421362f01e47809f0e18d3b4ccc
newTypeo <sinjh6348@naver.com>
2023-08-14 14:38:18 +0900

    첫 커밋

---
+++ src/main/java/com/dagachi/project/DagachiApplication.java
@@ -0,0 +1,13 @@
+package com.dagachi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagachiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagachiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dagachi/project/DagachiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dagachi.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagachiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
