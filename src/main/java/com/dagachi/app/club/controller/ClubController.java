aaa14304f5d05102edd29fb5aa42a5820f3ea499
newTypeo <sinjh6348@naver.com>
2023-08-15 17:59:48 +0900

    모임검색 ajax로 jsp까지 객체 가져옴

--- src/main/java/com/dagachi/app/club/controller/ClubController.java
+++ src/main/java/com/dagachi/app/club/controller/ClubController.java
@@ -3,6 +3,7 @@ package com.dagachi.app.club.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -51,8 +52,8 @@ public class ClubController {
 	public ResponseEntity<?> clubSearch(@RequestParam String keyword) {
 		
 		List<Club> clubs = clubService.clubSearch(keyword);
-		log.debug("clubs = {}", clubs);
-		return null;
+		// log.debug("clubs = {}", clubs);
+		return ResponseEntity.status(HttpStatus.OK).body(clubs);
 	}
 	
 	
