62650e006517bccdb2ae5659cd12be5e84cd9220
newTypeo <sinjh6348@naver.com>
2023-08-15 17:40:44 +0900

    .

--- src/main/java/com/dagachi/app/club/controller/ClubController.java
+++ src/main/java/com/dagachi/app/club/controller/ClubController.java
@@ -3,6 +3,7 @@ package com.dagachi.app.club.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.dagachi.app.club.entity.Club;
 import com.dagachi.app.club.service.ClubService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/club")
+@Slf4j
 public class ClubController {
 	
 	@Autowired
@@ -38,12 +42,12 @@ public class ClubController {
 	
 	
 	
-	// @RequestBody
 	@GetMapping("/clubSearch.do")
-	public void clubSearch(@RequestParam String keyword) {
+	public ResponseEntity<?> clubSearch(@RequestParam String keyword) {
 		
 		List<Club> clubs = clubService.clubSearch(keyword);
-		
+		log.debug("clubs = {}", clubs);
+		return null;
 	}
 	
 	
