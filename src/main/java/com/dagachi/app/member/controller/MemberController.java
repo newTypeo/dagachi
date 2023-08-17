4b8c5c13135ebe1b495784fb78bfca033dbc9d37
afteryoon <khsso102649@google.com>
2023-08-17 20:48:52 +0900

    sss

---
+++ src/main/java/com/dagachi/app/member/controller/MemberController.java
@@ -0,0 +1,64 @@
+package com.dagachi.app.member.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.dagachi.app.member.dto.MemberCreateDto;
+import com.dagachi.app.member.service.MemberService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Validated
+//@RequestMapping("/member")
+//@SessionAttributes({"loginMember"})
+@Slf4j
+public class MemberController {
+
+	@Autowired
+	private MemberService memberService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@GetMapping("/memberCreate.do")
+	public void memberCreate() {}
+	
+	@PostMapping("/memberCreate.do")
+	public String memberCreate(
+			@Valid MemberCreateDto member,
+			BindingResult bindingResult, 
+			RedirectAttributes redirectAttr) {
+		log.debug("member = {}", member);
+		
+		System.out.println(member);
+		
+		if(bindingResult.hasErrors()) {
+			ObjectError error = bindingResult.getAllErrors().get(0);
+			redirectAttr.addFlashAttribute("msg", error.getDefaultMessage());
+			return "redirect:/member/memberCreate.do";
+		} 
+		String rawPassword = member.getPassword();
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		log.debug("{} -> {}", rawPassword, encodedPassword);
+		member.setPassword(encodedPassword);
+		
+		int result = memberService.insertMember(member);
+		redirectAttr.addFlashAttribute("msg", "🎉🎉🎉 회원가입을 축하드립니다.🎉🎉🎉");
+		return "redirect:/";
+	}
+	
+
+	
+}
---
+++ src/main/java/com/dagachi/app/member/entity/Member.java
@@ -0,0 +1,33 @@
+package com.dagachi.app.member.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.dagachi.app.club.entity.Club;
+
+import lombok.Data;
+
+/**
+ * 나영
+ * */
+@Data
+public class Member {
+	
+	private String memberId;
+	private String password;
+	private String name;	
+	private String nickname;
+	private String phoneNo;	
+	private String email;	
+	private LocalDate birthday;	
+	private String gender;	
+	private String mbti;	
+	private String address;	
+	private int reportCount;	
+	private LocalDateTime enrollDate;	
+	private LocalDateTime withdrawalDate;
+	private LocalDateTime passwordChangeDate;	
+	private LocalDateTime lastLoginDate;
+	private String status; 
+
+}
