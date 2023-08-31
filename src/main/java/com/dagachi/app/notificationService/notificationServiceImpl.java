bf7a8a9314676d223b2104a29c9c7d73aba39ca5
afteryoon <khsso102649@google.com>
2023-08-31 15:31:31 +0900

    ++ 알람 수정중

--- src/main/java/com/dagachi/app/notificationService/notificationServiceImpl.java
+++ src/main/java/com/dagachi/app/notificationService/notificationServiceImpl.java
@@ -1,9 +1,16 @@
 package com.dagachi.app.notificationService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import com.dagachi.app.chat.entity.ChatLog;
+import com.dagachi.app.club.dto.ClubMemberAndImage;
+import com.dagachi.app.club.service.ClubService;
+import com.dagachi.app.ws.dto.Payload;
+import com.dagachi.app.ws.dto.PayloadType;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,5 +20,35 @@ public class notificationServiceImpl implements NotificationService {
 	
 	@Autowired
 	SimpMessagingTemplate simpMessagingTemplate;
+	
+	@Autowired
+	ClubService clubService;
+	
+	@Override
+	public int sendChatalarm(ChatLog chatlog) {
+	
+		
+		List<ClubMemberAndImage> members=clubService.findClubMembers(chatlog.getClubId());
+		String clubName= clubService.findClubInfoById(chatlog.getClubId()).getClubName();
+		
+		
+		
+		for(ClubMemberAndImage member : members) {
+			String to=member.getMemberId();
+			
+			Payload payload= Payload.builder()
+					.type(PayloadType.CHATNOTICE)
+					.to(to)
+					.from(chatlog.getWriter())
+					.content(clubName)
+					.build();
+			
+			simpMessagingTemplate.convertAndSend("/app/notice/" +to,payload);
+		
+		}
+
+		
+		return 0;
+	}
 	
 }
--- src/main/webapp/resources/js/stomp.js
+++ src/main/webapp/resources/js/stomp.js
@@ -16,6 +16,13 @@ stompClient.connect({}, (frame) => {
 			
 	});
 	
+	stompClient.subscribe(`/app/notice/${memberId}`, (message) => {
+			console.log(`/app/clubTalk/${memberId} : `, message);
+	
+		if(message.headers["content-type"])
+			renderMessage(message);
+	});
+	
 	
 });
 
@@ -27,37 +34,69 @@ const renderMessage = (message) => {
 	
 	
 	switch(type){
-	 	case "MOIMTALK":
-	 	const chatWrap =document.querySelector("#chatWrap");
-	 	const divbox=document.createElement('div');
-		let pro="";
+	
+		case "CHATNOTICE":
+			const alarmWrap=window.parent.document.querySelector("#alarmBox");
+			const roomMaps= window.parent.roomMaps;
+			console.log(roomMaps);
+			const chatRoomCheck=window.parent.document.querySelector("#content");
+			if(chatRoomCheck !== chatRoomCheck){
+				roomMaps.set(content, (0));
+				const alarmDiv=document.createElement('div');
+				alarmDiv.setAttribute('id', content);
+				alarmDiv.className = 'list-group';
+				alarmDiv.innerHTML=`
+					 <a href="#" class="list-group-item list-group-item-action list-group-item-light">
+						${content} :새로운 메세지가 도착하였습니다 
+					</a>
+					<span id="" class="badge badge-primary">
+						${roomMaps.get(content) || 1}
+					</span>
+				`;
+				alarmWrap.appendChild(alarmDiv);
+			}else{
+				roomMaps.set(content, (contentCountMap.get(content)) + 1)
+				window.parent.document.querySelector("#alarmBox").innerText=`
+						${roomMaps.get(content)}
+				`;
+			}
+			
 		
-		if(proList.length>0){
-			for(let i=0; i<proList.length; i++){
-			console.log("i번인덱스 확인",proList[i]["userProfileName"]);
-				if(proList[i]["userName"]===from){
-					pro=proList[i]["userProfileName"];
+		break;
+		
+		
+		
+	 	case "MOIMTALK":
+		 	const chatWrap =document.querySelector("#chatWrap");
+		 	const divbox=document.createElement('div');
+			let pro="";
+			
+			if(proList.length>0){
+				for(let i=0; i<proList.length; i++){
+				console.log("i번인덱스 확인",proList[i]["userProfileName"]);
+					if(proList[i]["userName"]===from){
+						pro=proList[i]["userProfileName"];
+					}
 				}
 			}
-		}
-		
-		if(pro === "")
-			pro=loadPro(from,to);
-		
-	 	if(from === memberId)
-	 		divbox.className = 'chat ch2';
-	 	else
-	 		divbox.className = 'chat ch1';
-	 		
-	 	divbox.innerHTML=`
-            <div class="icon"><i class="fa-solid fa-user"></i>
-           		<img alt="" src="${root}/resources/upload/member/profile/${pro}" class="resized-image" />
-            </div>
-            <div class="textbox">${content}</div>
-      	 	</div>
-	 	` ;
-	 	chatWrap.appendChild(divbox);
-      	 document.querySelector("#chatWrap").scrollTop = document.querySelector("#chatWrap").scrollHeight;
+			
+			if(pro === "")
+				pro=loadPro(from,to);
+			
+		 	if(from === memberId)
+		 		divbox.className = 'chat ch2';
+		 	else
+		 		divbox.className = 'chat ch1';
+		 		
+		 	divbox.innerHTML=`
+	            <div class="icon"><i class="fa-solid fa-user"></i>
+	           		<img alt="" src="${root}/resources/upload/member/profile/${pro}" class="resized-image" />
+	            </div>
+	            <div class="textbox">${content}</div>
+	      	 	</div>
+		 	` ;
+		 	chatWrap.appendChild(divbox);
+	      	 document.querySelector("#chatWrap").scrollTop = document.querySelector("#chatWrap").scrollHeight;
 	 	 break;
 	 	 
 	 	 
