14541cdcba27772d28a437660c745b0305ee861b
afteryoon <khsso102649@google.com>
2023-09-04 15:45:15 +0900

    세큐리티 수정

--- src/main/java/com/dagachi/app/config/WebSecurityConfig.java
+++ src/main/java/com/dagachi/app/config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/", "/index.jsp","/club/categoryList.do","/club/clubList.do","/admin/mainBannerList.do","/club/clubSearch.do").permitAll() //요청은 모든 사용자에게 허용
+			.antMatchers("/", "/index.jsp","/club/categoryList.do","/club/clubList.do","/admin/mainBannerList.do","/club/clubSearch.do","/member/memberCreate.do").permitAll() //요청은 모든 사용자에게 허용
 			.antMatchers("/member/memberKakaoCreate.do","/member/memberLogin.do","/member/searchId.do","/member/searchPw.do").anonymous()
 			.antMatchers("/admin/**").hasAuthority("ADMIN")
 			// .antMatchers("/member/memberCreate.do").anonymous() //로그인하지 않은 사용자만 가능
@@ -89,7 +89,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		    .tokenValiditySeconds(60 * 60 * 24 * 14); // 2주
 		
 		http.oauth2Login()
-			.loginPage("/member/memberLogin.do")
+			.loginPage("/index.jsp")
 			.userInfoEndpoint()
 			.userService(oauth2UserService);
 	}
